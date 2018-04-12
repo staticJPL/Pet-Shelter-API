10b7485f2f9f55547314d0755eaa0b8ea179eaba
StaticTJO <sk8terboy6931@gmail.com>
2018-04-12 01:05:18 -0600

    Server Complete, End points: Get All Pets, Get Pet by ID, and Post Creation given Name breed, location etc.

--- routes/insert.js
+++ routes/insert.js
@@ -2,14 +2,9 @@ var express = require('express');
 var query = require('../models/rest');
 var router = express.Router();
 
-var jsonPetObject = [{"pet_lookup_id":2,"name":"Katie",
-    "breed":"English Spaniel","type":"Springer",
-    "location":"Regina,SK","latitude":-32.0301,"longitude":-30.0123}];
-
-
-//TODO post function test with Postman
-router.get('/',function (req,res){
-
+router.post('/',function (req,res){
+    console.log(req.body);
+    jsonPetObject = req.body;
     query.addPet(jsonPetObject,function(error,results) {
         if(error){
             res.json({"status": 500, "error": error, "response": null});
